5db979ceed2834c9e1f9b14a04d598dc913eb555
Lázaro Alves <geo.eolo@gmail.com>
2021-07-10 16:37:56 -0300

    Adiciona componente utilizando estados

--- aula-hooks/src/Components/App/App.js
+++ aula-hooks/src/Components/App/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Counter from '../Counter/Counter';
+import SmartCounter from '../Smartcounter/Smartcounter';
+
 
 function App() {
   return (
+    <>
       <h1>Olá mundo</h1>
+      <SmartCounter/>
+    </>
   );
 }
 
