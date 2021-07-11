2ff29789a41d09db08b2082ed87b52884c31049e
Lázaro Alves <geo.eolo@gmail.com>
2021-07-10 21:02:53 -0300

    Adiciona estado dinamico ao componente

--- aula-hooks/src/Components/App/App.js
+++ aula-hooks/src/Components/App/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Counter from '../Counter/Counter';
 import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
 
 
 function App() {
   return (
     <>
       <h1>Olá mundo</h1>
-      <SmartCounter/>
+      <Ifoodcounter/>
     </>
   );
 }
